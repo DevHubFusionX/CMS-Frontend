439a2cf7d6b9336b6391ab315174ce83714f2cfb
Franklin Fanya <franklinfanya@gmail.com>
2025-08-04 12:45:22 +0100

    fixed2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+
 import HubFusionXLoader from "./Components/Common/HubFusionXLoader";
 import PreloadCritical from "./Components/Common/PreloadCritical";
 
