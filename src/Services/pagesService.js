a481994257d3b2bc790313035005278f108cf287
Franklin Fanya <franklinfanya@gmail.com>
2025-08-08 20:33:11 +0100

    fix2

--- src/Services/pagesService.js
+++ src/Services/pagesService.js
@@ -1,21 +1,21 @@
-import { apiClient } from './apiClient';
+import api from './apiClient';
 
 export const pagesService = {
   // Get all published pages
-  getAllPages: () => apiClient.get('/pages'),
+  getAllPages: () => api.get('/pages'),
   
   // Get all pages (admin)
-  getAllPagesAdmin: () => apiClient.get('/pages/all'),
+  getAllPagesAdmin: () => api.get('/pages/all'),
   
   // Get page by slug
-  getPageBySlug: (slug) => apiClient.get(`/pages/slug/${slug}`),
+  getPageBySlug: (slug) => api.get(`/pages/slug/${slug}`),
   
   // Create new page
-  createPage: (pageData) => apiClient.post('/pages', pageData),
+  createPage: (pageData) => api.post('/pages', pageData),
   
   // Update page
-  updatePage: (id, pageData) => apiClient.put(`/pages/${id}`, pageData),
+  updatePage: (id, pageData) => api.put(`/pages/${id}`, pageData),
   
   // Delete page
-  deletePage: (id) => apiClient.delete(`/pages/${id}`)
+  deletePage: (id) => api.delete(`/pages/${id}`)
 };
